e3fd7b36ef062aebc4e92595d995c48ddb97fc7c
hacheG <943@holbertonschool.com>
2019-06-19 23:31:47 +0000

    test6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-printf("Size of a char: %d", sizeof(char));
-printf("Size of an int: %d", sizeof(int));
-printf("Size of a long int: %d", sizeof(long int));
-printf("Size of a long long int: %d", sizeof(long long int));
-printf("Size of a float: %d", sizeof(float));
+printf("Size of a char: %d\n", sizeof(char));
+printf("Size of an int: %d\n", sizeof(int));
+printf("Size of a long int: %d\n", sizeof(long int));
+printf("Size of a long long int: %d\n", sizeof(long long int));
+printf("Size of a float: %d\n", sizeof(float));
 
 return (0);
 }
