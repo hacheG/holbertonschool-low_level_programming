b05261627dd4016cc5efc2bdc075c33ecc7f275b
hacheG <943@holbertonschool.com>
2019-06-20 23:18:28 +0000

    task10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - more headers goes there
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 49;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		for (j = 48 ; j < 58 ; j++)
+		{
+			if (i == 57 && j == 57)
+			{
+				putchar (i);
+				putchar (j);
+				break;
+			}
+			putchar (i);
+			putchar (j);
+			putchar (',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
