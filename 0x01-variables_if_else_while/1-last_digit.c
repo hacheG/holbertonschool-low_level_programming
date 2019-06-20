64cff079d01f84fb9996e67a3c1c05005803607f
hacheG <943@holbertonschool.com>
2019-06-20 23:53:07 +0000

    10

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 	a = n % 10;
 	if (a > 5)
 	{
-		printf("last digit of %d in %d and is greater than 5\n", n, a);
+		printf("last digit of %d is %d and is greater than 5\n", n, a);
 	}
 	if (a == 0)
 	{
-		printf("last digit of %d in %d and is 0\n", n, a);
+		printf("last digit of %d is %d and is 0\n", n, a);
 	}
 	if (a < 6 && a != 0)
 	{
-		printf("last digit of %d in %d and is less than 6 and not 0\n", n, a);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 return (0);
 }
