c6c0cc3c496c1d60ad624cdffee081410a394ce8
hacheG <943@holbertonschool.com>
2019-06-20 01:20:23 +0000

    test101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 57);
+write(1, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 58);
 return (1);
 }
