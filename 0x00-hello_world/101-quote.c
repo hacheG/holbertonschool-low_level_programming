c74e84de9ac75a57060e1c3b4731639bea16f20b
hacheG <943@holbertonschool.com>
2019-06-20 01:05:09 +0000

    test101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Kopar, 2015-10-19", 57);
+return (1);
+}
