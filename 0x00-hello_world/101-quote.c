e715a8e5e58100d6c3bb18f1f004f34a42411d1b
hacheG <943@holbertonschool.com>
2019-06-20 01:32:25 +0000

    test101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
 
 return (1);
 }
