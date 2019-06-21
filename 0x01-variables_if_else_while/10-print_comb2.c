fd0a78b7f65fe2c11b410a67e1aa9a30bfd62093
hacheG <943@holbertonschool.com>
2019-06-21 00:29:52 +0000

    10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,24 +7,22 @@
  */
 int main(void)
 {
-        int i ;
-        int j ;
+	int i;
+	int j;
 
-        for (i = 48 ; i < 58 ; i++)
-        {
-            
+	for (i = 48 ; i < 58 ; i++)
+	{    
 		for (j = 48 ; j < 58 ; j++)
-                {
-                        putchar (i);
-                        putchar (j);
-                        if (i != 57 || j != 57)
-                        {
-                                putchar (',');
-                                putchar (' ');
-                        }
+		{
+			putchar (i);
+			putchar (j);
+			if (i != 57 || j != 57)
+			{
+				putchar (',');
+				putchar (' ');
+			}
 		}
-        
-        }
+	}
 	putchar ('\n');
 	return (0);
 }
