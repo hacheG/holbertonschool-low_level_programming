dffccb507fee12bdb766591c2e4281338de90d8b
hacheG <943@holbertonschool.com>
2019-06-25 22:18:29 +0000

    ultimoholb

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - This is a description
+ * @n: The number to be checked
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 99; i++)
+	{
+		printf("%d, ", i);
+	}
+}
