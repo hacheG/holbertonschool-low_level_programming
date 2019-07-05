980bb22f2537a5aa0927612176c6c7e8d09da7b4
Hache <943@holbertonschool.com>
2019-07-05 11:05:42 -0500

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* _strncat - concat with a character
+* @dest: dest
+* @src: to concate
+* @n: num to characters
+* Return: concat string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+		i++;
+	while (j < n && src[j] != '\0')
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (dest);
