22f45fc5128859d2ab0a1593dcfc2909f267194f
hacheG <943@holbertonschool.com>
2019-07-09 00:01:47 -0500

    2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,17 @@
 #include "holberton.h"
-
 /**
- * _memset - assing memory
- * @s: buffer
- * @b: value to set
- * @n: n space to set
- * Return: change buffer
- */
-
+* _memset - entry point
+* Description: sets the memory of the bytes.
+* @b: n-val.
+* @s: f-byte.
+* @n: c-bytes.
+* Return: Pointer s.
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int mem;
 
-	while (i < n)
-	{
-		*s = b;
-		s++;
-		i++;
-	}
-
+	for (mem = 1; mem <= n; mem++)
+		*(s + (mem - 1)) = b;
 	return (s);
 }
