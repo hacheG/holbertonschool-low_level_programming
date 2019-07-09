e6f1a9e891f9fb05996001361151105feaa9eff4
hacheG <943@holbertonschool.com>
2019-07-08 23:25:05 -0500

    2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
 /**
- 3 3 * _memset - Makes the sum of two numbers
- 4 4 * @s: First operand
- 5 5 * @b: Second operand
- 6 6 * @n: Second operand
- 7 7 *
- 8 8 * Return: The sum of the two parameters
- 9 9 */
+ * _memset - Makes the sum of two numbers
+ * @s: First operand
+ * @b: Second operand
+ * @n: Second operand
+ *
+ * Return: The sum of the two parameters */
 
 char *_memset(char *s, char b, unsigned int n)
 
