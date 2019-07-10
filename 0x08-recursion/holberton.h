d8c0bcff485dd31157d357825d7b30ea092a2f91
hacheG <943@holbertonschool.com>
2019-07-10 21:29:12 +0000

    fix task1 and holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,7 +1,7 @@
 #ifndef holberton_h
 #define holberton_h
 int _putchar(char c);
-void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
