cc4a7340bd4ceca59a1446179ba6dc4c367380e3
hacheG <943@holbertonschool.com>
2019-07-16 17:51:35 +0000

    task_and_directory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - writes the character c to stdout
+ * @size: The character to print
+ * @c: The character to print
+ * Return: pointer to array or null
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *q;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		q = malloc(sizeof(char) * size);
+
+		for (i = 0; i < size; i++)
+		{
+			q[i] = c;
+		}
+
+		return (q);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+0. Float like a butterfly, sting like a bee
+1. The woman who has no imagination has no wings
+2. He who is not courageous enough to take risks will accomplish nothing in life
+3. If you even dream of beating me you'd better wake up and apologize
+4. It's not bragging if you can back it up
+5. It isn't the mountains ahead to climb that wear you out; it's the pebble
+in your shoe
