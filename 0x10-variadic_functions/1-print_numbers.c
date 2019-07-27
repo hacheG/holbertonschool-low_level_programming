7a5a6292055dc1b228f516cbb376fc1483022110
hacheG <943@holbertonschool.com>
2019-07-27 19:43:20 +0000

    fix_task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator == NULL)
 			return;
 
-		else if (i != n-1)
+		else if (i != n - 1)
 			printf("%s", separator);
 	}
 	va_end(printed);
