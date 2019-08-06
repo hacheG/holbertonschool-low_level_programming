bd1ea60de01d4ea0c7538269de125af9235038f8
hacheG <943@holbertonschool.com>
2019-08-06 17:37:04 -0500

    [FEATURE] task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * reverse_listint - Reverses a singly linked list
+ * @head: pointer to the first element of the reversed list
+ * Return: A pointer to the first element of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *new_tmp;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	tmp = *head;
+	*head = tmp->next;
+	tmp->next = NULL;
+
+	while (1 && *head != NULL)
+	{
+		new_tmp = (*head)->next;
+		(*head)->next = tmp;
+		tmp = *head;
+		if (new_tmp == NULL)
+			return (*head);
+		*head = new_tmp;
+	}
+	if (*head == NULL)
+	{
+		*head = tmp;
+		return (*head);
+	}
+	return (NULL);
+}
