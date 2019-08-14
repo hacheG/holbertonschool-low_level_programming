a7c05df56652b45df69eceece92004098c3b5ead
hacheG <943@holbertonschool.com>
2019-08-13 23:28:22 -0500

    fix_header

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -13,5 +13,12 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void read_elf_header(int32_t fd, Elf32_Ehdr *elf_header);
+bool is_ELF(Elf32_Ehdr eh);
+void print_os(Elf32_Ehdr eh);
+void print_typ(Elf32_Ehdr eh);
+void print_clas(Elf32_Ehdr eh);
+void print_dta(Elf32_Ehdr eh);
+void print_vrsn(Elf32_Ehdr eh);
+void print_elf_header(Elf32_Ehdr elf_header);
 #endif
