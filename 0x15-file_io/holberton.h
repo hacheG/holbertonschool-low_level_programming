62900def41fbaf29c0a9e8162bfd1d77994aef4c
hacheG <943@holbertonschool.com>
2019-08-13 23:31:18 -0500

    fix_header

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -8,7 +8,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-
+#include <stdbool.h>
+#include <elf.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
