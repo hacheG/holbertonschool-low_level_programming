76108788470746be3dad4f1d6b4a9b373147c7b6
hacheG <943@holbertonschool.com>
2019-09-16 17:25:20 -0500

    fix1Task6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,5 +6,16 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int count = 0;
 
+	if (head == NULL)
+		return (0);
+	while (head->prev != NULL)
+		head = head->prev;
+	while (head != NULL)
+	{
+		count = count + head->n;
+		head = head->next;
+	}
+	return (count);
 }
