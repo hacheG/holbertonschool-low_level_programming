14fb61372d2907b51dd626f79af2da71d73f4667
hacheG <943@holbertonschool.com>
2019-09-16 18:26:51 -0500

    fix1Task8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,5 +7,26 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *tmp_del, *tmp_head;
 
+	if (!head || !*head)
+		return (-1);
+	tmp_head = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+		free(tmp_head);
+		return (1);
+	}
+	for ( ; index > 1 && tmp_head && tmp_head->next; index--)
+		tmp_head = tmp_head->next;
+	if (!tmp_head->next)
+		return (-1);
+	tmp_del = tmp_head->next;
+	tmp_head->next = tmp_del->next ? tmp_del->next : NULL;
+	if (tmp_del->next)
+		tmp_del->next->prev = tmp_head;
+	free(tmp_del);
+	return (1);
 }
