938005e92173507199c80a9636b18629eb62634b
hacheG <943@holbertonschool.com>
2019-09-16 12:32:38 -0500

    fix1Task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,5 +6,17 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+	while ((*h).prev != NULL)
+		h = (*h).prev;
+	while (h != NULL)
+	{
+		printf("%d\n", (*h).n);
+		h = (*h).next;
+		count++;
+	}
+	return (count);
 }
