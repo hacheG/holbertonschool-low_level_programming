30abc2138e8173afe8c3a9b3e2293cdd8a220244
hacheG <943@holbertonschool.com>
2019-09-16 18:14:07 -0500

    fix3Task5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,10 +9,13 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *temp = head;
 	unsigned int count = 0;
+
 	if (head == NULL)
 		return (NULL);
 	while (count != index)
 	{
+		if (temp == NULL)
+			return (NULL);
 		count++;
 		temp = temp->next;
 	}
