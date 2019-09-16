ca84018c26023c227d5e8759ecce072b492f1340
hacheG <943@holbertonschool.com>
2019-09-16 17:59:35 -0500

    fix1Task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,5 +8,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *copy, *temp, *new;
+	unsigned int i = 0;
 
+	copy = *h;
+	if (h == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	if (idx == 0 && *h == NULL)
+	{
+		new->next = copy, *h = new;
+		return (*h);
+	}
+	while (copy != NULL)
+	{
+		if (idx == 0)
+		{
+			new->next = *h, copy->prev = new;
+			new->prev = NULL, *h = new;
+			return (*h);
+		}
+		if (i == (idx - 1))
+		{
+			if (copy->next == NULL)
+			{
+				new->next = NULL, new->prev = copy;
+				copy->next = new;
+				return (new);
+			}
+			temp = copy->next, new->next = temp;
+			temp->prev = new, new->prev = copy;
+			copy->next = new;
+			return (new);
+		}
+		i++;
+		copy = copy->next;
+	}
+	return (NULL);
 }
