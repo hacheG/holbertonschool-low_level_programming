97ba41bf01cb08853d79e93ba6b8ced40c83e6b1
hacheG <943@holbertonschool.com>
2019-09-16 13:52:19 -0500

    fix1Task2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,5 +7,15 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+		return (NULL);
+	(*new_node).n = n;
+	(*new_node).next = *head;
+	(*new_node).prev = NULL;
+	if (*head != NULL)
+		(**head).prev = new_node;
+	*head = new_node;
+	return (*head);
 }
