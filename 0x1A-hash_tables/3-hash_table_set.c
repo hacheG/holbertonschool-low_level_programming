b5d861b83d33ce4cc8d7e5f0691233474024ad1a
hacheG <943@holbertonschool.com>
2019-10-11 21:06:58 -0500

    fix6Task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	size = ht->size;
 	index = key_index((const unsigned char *)key, size);
-	if (ht->array[index] != NULL && strcpy(ht->array[index]->key, key) == 0)
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
 	{
 		ht->array[index]->value = strdup(value);
 		return (1);
