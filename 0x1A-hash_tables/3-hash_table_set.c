566128e3883dbc4bb40f7c3e6ccc57a4c890c2ee
hacheG <943@holbertonschool.com>
2019-10-11 20:46:08 -0500

    fix4Task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, size;
 	hash_node_t *newNode;
 
+	if (ht == NULL || key == NULL || value == NULL)
+	{
+		return (0);
+	}
+
 	size = ht->size;
 	index = key_index((const unsigned char *)key, size);
 	if (ht->array[index] != NULL && ht->array[index]->key != NULL)
