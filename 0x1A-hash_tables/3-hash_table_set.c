c0a63ac33355753b6344c76a19fe268fcca32f21
hacheG <943@holbertonschool.com>
2019-10-11 20:24:36 -0500

    fix2Task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,18 +10,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, size;
-	hash_node_t *newNode = (hash_node_t *)malloc(sizeof(hash_node_t));
-
-	if (newNode == NULL)
-	{
-		return (0);
-	}
+	hash_node_t *newNode;
 
 	size = ht->size;
 	index = key_index((const unsigned char *)key, size);
-	ht->array[index]->value = strdup(value);
-	return (1);
+	
+	if (ht->array[index] != NULL && ht->array[index]->key != NULL) 
+	{
+		ht->array[index]->value = strdup(value);
+		return (1);
+	}
+	
+	else
+	{
+		newNode = (hash_node_t *)malloc(sizeof(hash_node_t));
 
+		if (newNode == NULL)
+		{
+			return (0);
+		}
 
-
+		newNode->key = strdup(key);
+		newNode->value = strdup(value);
+		newNode->next = ht->array[index];
+		return (1);
+	}
 }
