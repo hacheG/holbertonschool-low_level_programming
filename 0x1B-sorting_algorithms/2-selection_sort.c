c627e0ec68df7ba9efdf9ab3b2903b78f8ab4e45
hacheG <943@holbertonschool.com>
2019-10-29 23:17:52 +0000

    fix_5

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -20,7 +20,7 @@ void selection_sort(int *array, size_t size)
 		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min])
+			if (array[min] > array[j])
 			{
 				min = j;
 			}
