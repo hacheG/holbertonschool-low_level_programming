88d05ba1131fbb3e3cd267646a94b3e4d9a2e14f
hacheG <943@holbertonschool.com>
2019-10-30 00:16:27 +0000

    fix6_task3

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -30,7 +30,7 @@ int partition(int arr[], int low, int high, size_t size)
 	int i = (low - 1);
 	int j;
 
-	for (j = low; j < high - 1; j++)
+	for (j = low; j <= high - 1; j++)
 	{
 		if (arr[j] < pivot)
 		{
