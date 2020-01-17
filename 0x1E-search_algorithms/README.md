29f71869fa6f749f42c63c8d873fea5a1f392fed
hacheG <943@holbertonschool.com>
2020-01-16 21:22:06 -0500

    [LAST_C] finally

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,3 @@
+# 0x1E. C - Search Algorithms
+## 0. Linear search mandatory
+Write a function that searches for a value in an array of integers using the Linear search algorithm
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
